3008b4e5be0e3caf38c12bb2b36793c07de0862e
Mikolaj Gawlik <mikolaj3313@gmail.com>
2024-03-02 13:09:36 +0100

    Fixed background image shift while opening search bar

--- components/BackgroundImage.tsx
+++ components/BackgroundImage.tsx
@@ -9,8 +9,7 @@ interface Props {
 
 const BackgroundImage = ({ cover, banner, alt }: Props) => {
   return (
-    // <div className="absolute w-screen aspect-square md:aspect-[2/1] lg:aspect-[3/1] xl:aspect-[4/1] overflow-hidden -z-10">
-    <div className="absolute w-full h-[75%] overflow-hidden -z-10">
+    <div className="absolute w-full overflow-hidden -z-10 h-[40vh] md:h-[60vh] lg:h-[75vh]">
       {(banner || cover) && (
         <Image
           alt={`Tło dla ${alt}`}
